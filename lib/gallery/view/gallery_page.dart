081819d73cace6a9dee49a26627e0111d1eb3196
GiudaBallerino <lorispesarin@gmail.com>
2022-05-20 12:46:55 +0200

    added image PageView

--- lib/gallery/view/gallery_page.dart
+++ lib/gallery/view/gallery_page.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
+import 'package:crop_image/crop_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gestione_immagini/app/app.dart';
 import 'package:gestione_immagini/gallery/gallery.dart';
 import 'package:imgs_repository/imgs_repository.dart';
+import 'package:photo_view/photo_view.dart';
 
 class GalleryPage extends StatelessWidget {
   const GalleryPage({Key? key}) : super(key: key);
@@ -23,17 +28,13 @@ class GalleryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("l10n.todosOverviewAppBarTitle"),
-        actions: const [
-        ],
-      ),
       body: MultiBlocListener(
         listeners: [
           BlocListener<GalleryBloc, GalleryState>(
             listenWhen: (previous, current) =>
-            previous.status != current.status,
+                previous.status != current.status,
             listener: (context, state) {
               if (state.status == GalleryStatus.failure) {
                 ScaffoldMessenger.of(context)
@@ -48,18 +49,17 @@ class GalleryView extends StatelessWidget {
           ),
           BlocListener<GalleryBloc, GalleryState>(
             listenWhen: (previous, current) =>
-            previous.lastDeletedImg != current.lastDeletedImg &&
+                previous.lastDeletedImg != current.lastDeletedImg &&
                 current.lastDeletedImg != null,
             listener: (context, state) {
-              final deletedImg= state.lastDeletedImg!;
+              final deletedImg = state.lastDeletedImg!;
               final messenger = ScaffoldMessenger.of(context);
               messenger
                 ..hideCurrentSnackBar()
                 ..showSnackBar(
                   SnackBar(
                     content: Text(
-                        "l10n.todosOverviewTodoDeletedSnackbarText(deletedTodo.title,),"
-                    ),
+                        "l10n.todosOverviewTodoDeletedSnackbarText(deletedTodo.title,),"),
                     action: SnackBarAction(
                       label: "l10n.todosOverviewUndoDeletionButtonText",
                       onPressed: () {
@@ -90,41 +90,28 @@ class GalleryView extends StatelessWidget {
                 );
               }
             }
-            return CupertinoScrollbar(
-              child: ListView(
-                children: [
-
-                  for(int i=0;i<10;i++)
-                    Container(
-                      width: 300,
-                      height: 300,
-                      color: Colors.red,
+            return PageView.builder(
+              itemCount: state.filteredImgs.length,
+              scrollDirection: Axis.vertical,
+              pageSnapping: true,
+              itemBuilder: (context, index) {
+                final controller = CropController(
+                  aspectRatio: 1,
+                  defaultCrop: const Rect.fromLTRB(0.1, 0.1, 0.9, 0.9),
+                );
+                return Center(
+                  child: CropImage(
+                    controller: controller,
+                    image: Image.file(
+                      File(
+                        state.filteredImgs.toList()[index].path,
+                      ),
+                      fit: BoxFit.contain,
                     ),
-                  //for (final img in state.filteredImgs)
-                  // TodoListTile(
-                  //   todo: todo,
-                  //   onToggleCompleted: (isCompleted) {
-                  //     context.read<TodosOverviewBloc>().add(
-                  //       TodosOverviewTodoCompletionToggled(
-                  //         todo: todo,
-                  //         isCompleted: isCompleted,
-                  //       ),
-                  //     );
-                  //   },
-                  //   onDismissed: (_) {
-                  //     context
-                  //         .read<TodosOverviewBloc>()
-                  //         .add(TodosOverviewTodoDeleted(todo));
-                  //   },
-                  //   onTap: () {
-                  //     Navigator.of(context).push(
-                  //       EditTodoPage.route(initialTodo: todo),
-                  //     );
-                  //   },
-                  // ),
-
-                ],
-              ),
+                    onCrop: (rect) => print(rect),
+                  ),
+                );
+              },
             );
           },
         ),
@@ -132,3 +119,44 @@ class GalleryView extends StatelessWidget {
     );
   }
 }
+
+// return ListView.builder(
+//   controller: ScrollController(),
+//   physics: PageScrollPhysics(),
+//   scrollDirection: Axis.vertical,
+//   itemCount: state.filteredImgs.length,
+//   itemBuilder: (context, index) {
+//     return Container(
+//       width: size.width,
+//       height: size.height,
+//       child: Image.file(
+//         File(
+//           state.filteredImgs.toList()[index].path,
+//         ),
+//         fit: BoxFit.contain,
+//       ),
+//     );
+//   },
+// );
+//for (final img in state.filteredImgs)
+// TodoListTile(
+//   todo: todo,
+//   onToggleCompleted: (isCompleted) {
+//     context.read<TodosOverviewBloc>().add(
+//       TodosOverviewTodoCompletionToggled(
+//         todo: todo,
+//         isCompleted: isCompleted,
+//       ),
+//     );
+//   },
+//   onDismissed: (_) {
+//     context
+//         .read<TodosOverviewBloc>()
+//         .add(TodosOverviewTodoDeleted(todo));
+//   },
+//   onTap: () {
+//     Navigator.of(context).push(
+//       EditTodoPage.route(initialTodo: todo),
+//     );
+//   },
+// ),
