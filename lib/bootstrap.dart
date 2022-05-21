ea43c1e2085fb7e2e250ea44fa5a40c95db75c56
GiudaBallerino <lorispesarin@gmail.com>
2022-05-21 22:34:19 +0200

    added tagRepository to the parameters

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -5,22 +5,25 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 import 'package:img_api/img_api.dart';
 import 'package:imgs_repository/imgs_repository.dart';
+import 'package:tag_api/tag_api.dart';
+import 'package:tags_repository/tags_repository.dart';
 
 import 'app/app.dart';
 import 'app/app_bloc_observer.dart';
 
-void bootstrap({required ImgApi imgApi}) {
+void bootstrap({required ImgApi imgApi,required TagApi tagApi}) {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   final imgsRepository = ImgsRepository(imgApi: imgApi);
+  final tagsRepository =TagsRepository(tagApi: tagApi);
 
   runZonedGuarded(
         () async {
       await BlocOverrides.runZoned(
             () async => runApp(
-          App(imgsRepository: imgsRepository),
+          App(imgsRepository: imgsRepository,tagsRepository:tagsRepository),
         ),
         blocObserver: AppBlocObserver(),
       );
