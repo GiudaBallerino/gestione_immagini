8b6eea51ec92bafcaf6fe92f9ec5549848be9707
GiudaBallerino <lorispesarin@gmail.com>
2022-05-21 18:17:48 +0200

    added methods for selections

--- packages/img_api/lib/src/img_api.dart
+++ packages/img_api/lib/src/img_api.dart
@@ -1,4 +1,5 @@
 import 'package:img_api/img_api.dart';
+import 'package:selection_api/selection_api.dart';
 
 /// {@template todos_api}
 /// The interface for an API that provides access to a list of todos.
@@ -10,10 +11,18 @@ abstract class ImgApi {
   /// Provides a [Stream] of all imgs.
   Stream<List<Img>> getImgs();
 
+  /// Provides a [List] of all selections.
+  Stream<List<Selection>> getSelections(String path);
+
   /// Saves a [img].
   ///
   /// If a [img] with the same name already exists, it will be replaced.
   Future<void> saveImg(Img image);
+
+  /// Saves a [selection].
+  ///
+  /// If a [selection] with the same id already exists, it will be replaced.
+  Future<void> saveSelection(String path, Selection selection);
 
   /// Deletes the img with the given path.
   ///
@@ -21,7 +30,16 @@ abstract class ImgApi {
   /// thrown.
   Future<void> deleteImg(String path);
 
+  /// Deletes the selection with the given id.
+  ///
+  /// If no selection with the given id exists, a [SelectionNotFoundException] error is
+  /// thrown.
+  Future<void> deleteSelection(String path, String id);
+
 }
 
 /// Error thrown when a [Img] with a given path is not found.
 class ImgNotFoundException implements Exception {}
+
+/// Error thrown when a [Selection] with a given id is not found.
+class SelectionNotFoundException implements Exception {}
--- packages/imgs_repository/lib/src/imgs_repository.dart
+++ packages/imgs_repository/lib/src/imgs_repository.dart
@@ -1,6 +1,7 @@
 import 'package:img_api/img_api.dart';
+import 'package:selection_api/selection_api.dart';
 
-/// {@template todos_repository}
+/// {@template imgs_repository}
 /// A repository that handles img related requests.
 /// {@endtemplate}
 class ImgsRepository {
@@ -14,14 +15,28 @@ class ImgsRepository {
   /// Provides a [Stream] of all imgs.
   Stream<List<Img>> getImgs() => _imgApi.getImgs();
 
+  /// Provides a [Stream] of all selections.
+  Stream<List<Selection>> getSelections(String path) => _imgApi.getSelections(path);
+
   /// Saves a [img].
   ///
   /// If a [img] with the same path already exists, it will be replaced.
   Future<void> saveImg(Img img) => _imgApi.saveImg(img);
+
+  /// Saves a [selection].
+  ///
+  /// If a [selection] with the same id already exists, it will be replaced.
+  Future<void> saveSelection(String path, Selection selection) => _imgApi.saveSelection(path, selection);
 
   /// Deletes the img with the given path.
   ///
   /// If no img with the given path exists, a [ImgNotFoundException] error is
   /// thrown.
   Future<void> deleteImg(String path) => _imgApi.deleteImg(path);
+
+  /// Deletes the selection with the given id.
+  ///
+  /// If no selection with the given id exists, a [SelectionNotFoundException] error is
+  /// thrown.
+  Future<void> deleteSelection(String path, String id) => _imgApi.deleteSelection(path,id);
 }
