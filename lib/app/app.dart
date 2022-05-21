93c0c84fb56667462668f6a16d63c929dc89e6ef
GiudaBallerino <lorispesarin@gmail.com>
2022-05-21 22:33:39 +0200

    added MultiRepositoryProvider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:imgs_repository/imgs_repository.dart';
+import 'package:tags_repository/tags_repository.dart';
 
 import '../home/view/home_page.dart';
 
 
 class App extends StatelessWidget {
-  const App({Key? key,required this.imgsRepository}) : super(key: key);
+  const App({Key? key,required this.imgsRepository,required this.tagsRepository}) : super(key: key);
 
   final ImgsRepository imgsRepository;
+  final TagsRepository tagsRepository;
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider.value(
-      value: imgsRepository,
+
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<ImgsRepository>(create: (context) => imgsRepository),
+        RepositoryProvider<TagsRepository>(create: (context) => tagsRepository),
+      ],
       child: const AppView(),
     );
   }
