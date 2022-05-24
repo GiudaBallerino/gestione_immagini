c104996e791b3fa830a04198daf40097e17c77cc
GiudaBallerino <lorispesarin@gmail.com>
2022-05-24 17:22:14 +0200

    added some state

--- lib/gallery/bloc/gallery_state.dart
+++ lib/gallery/bloc/gallery_state.dart
@@ -6,29 +6,48 @@ class GalleryState extends Equatable {
   const GalleryState({
     this.status = GalleryStatus.initial,
     this.images = const [],
-    this.filter = GalleryFilter.all,
+    this.tags = const [],
+    this.actualIndex = 0,
+    this.currentlyDraggedIndex = -1,
+    this.points= const [Offset(500,500),Offset(600,500), Offset(600,600),Offset(500,600)],
     this.lastDeletedImg,
+    this.selecting = false,
   });
 
   final GalleryStatus status;
-  final List<Img> images;
-  final GalleryFilter filter;
-  final Img? lastDeletedImg;
 
-  Iterable<Img> get filteredImgs => filter.applyAll(images);
+  final List<Img> images;
+  final List<Tag> tags;
+
+  final bool selecting;
+  final int actualIndex;
+  final int currentlyDraggedIndex;
+  final List<Offset> points;
+
+  final Img? lastDeletedImg;
 
   GalleryState copyWith({
     GalleryStatus Function()? status,
     List<Img> Function()? images,
-    GalleryFilter Function()? filter,
+    List<Tag> Function()? tags,
+    int Function()? actualIndex,
+    int Function()? currentlyDraggedIndex,
+    List<Offset> Function()? points,
     Img? Function()? lastDeletedImg,
+    bool Function()? selecting,
   }) {
     return GalleryState(
       status: status != null ? status() : this.status,
       images: images != null ? images() : this.images,
-      filter: filter != null ? filter() : this.filter,
+      tags: tags != null ? tags() : this.tags,
+      points: points != null ? points() : this.points,
+      actualIndex: actualIndex != null ? actualIndex() : this.actualIndex,
+      currentlyDraggedIndex: currentlyDraggedIndex != null
+          ? currentlyDraggedIndex()
+          : this.currentlyDraggedIndex,
       lastDeletedImg:
           lastDeletedImg != null ? lastDeletedImg() : this.lastDeletedImg,
+      selecting: selecting != null ? selecting() : this.selecting,
     );
   }
 
@@ -36,7 +55,11 @@ class GalleryState extends Equatable {
   List<Object?> get props => [
         status,
         images,
-        filter,
+        tags,
+        actualIndex,
+        currentlyDraggedIndex,
+        points,
         lastDeletedImg,
+        selecting,
       ];
 }
