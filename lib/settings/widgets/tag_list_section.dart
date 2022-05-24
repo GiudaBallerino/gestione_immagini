4f7d8fcfc3a7adf70648eef8940a194ff538c9cc
GiudaBallerino <lorispesarin@gmail.com>
2022-05-24 17:23:44 +0200

    minor changes

--- lib/settings/widgets/tag_list_section.dart
+++ lib/settings/widgets/tag_list_section.dart
@@ -16,54 +16,41 @@ class TagListSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextEditingController _controller = TextEditingController();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
       children: [
-        Padding(
-          padding: EdgeInsets.only(bottom: 10),
-          child: Text(
-            'Lista dei tag:',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        for (final tag in tags)
+          InputChip(
+            label: Text(tag.name),
+            onDeleted: () => onDeleted(tag),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
           ),
-        ),
-        Wrap(
-          spacing: 5,
-          children: [
-            for (final tag in tags)
-              InputChip(
-                label: Text(tag.name),
-                onDeleted: () => onDeleted(tag),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 13),
-              ),
-            IntrinsicWidth(
-              child: TextField(
-                controller: _controller,
-                style: TextStyle(fontSize: 14),
-                decoration: InputDecoration(
-                  hintText: 'tag name',
-                  isDense: true,
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  suffixIcon: InkWell(
-                    onTap: () {
-                      if (!_controller.text.isEmpty) onSubmit(_controller.text);
-                    },
-                    child: Icon(
-                      Icons.add_circle,
-                    ),
-                  ),
-                ),
-                onSubmitted: (text) {
+        IntrinsicWidth(
+          child: TextField(
+            controller: _controller,
+            style: TextStyle(fontSize: 14),
+            decoration: InputDecoration(
+              hintText: 'tag name',
+              isDense: true,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+              suffixIcon: InkWell(
+                onTap: () {
                   if (!_controller.text.isEmpty) onSubmit(_controller.text);
                 },
+                child: Icon(
+                  Icons.add_circle,
+                ),
               ),
             ),
-          ],
-        )
+            onSubmitted: (text) {
+              if (!_controller.text.isEmpty) onSubmit(_controller.text);
+            },
+          ),
+        ),
       ],
     );
   }
