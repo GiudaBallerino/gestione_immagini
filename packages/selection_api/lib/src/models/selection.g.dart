b1cae6a70b99ab238cf8e3730540da3b694aedc2
GiudaBallerino <lorispesarin@gmail.com>
2022-05-25 14:45:32 +0200

    fix tag toJson

--- packages/selection_api/lib/src/models/selection.g.dart
+++ packages/selection_api/lib/src/models/selection.g.dart
@@ -12,8 +12,13 @@ Selection _$SelectionFromJson(Map<String, dynamic> json) => Selection(
     points: List.from(json['points']).map((item)=>Offset(item['dx'] as double ,item["dy"] as double)).toList(),
 );
 
+Map<String, dynamic> offsetToJson(Offset offset) => <String, double>{
+  'dx': offset.dx,
+  'dy': offset.dy,
+};
+
 Map<String, dynamic> _$SelectionToJson(Selection instance) => <String, dynamic>{
   'id': instance.id,
-  'tag': instance.tag,
-  'points': instance.points,
+  'tag': instance.tag.toJson(),
+  'points': instance.points.map((p)=>offsetToJson(p)).toList(),
 };
